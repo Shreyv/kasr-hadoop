0ef1e94bc00cd5557a74f568f91dc5e9524f0178
shrey <shreyv991995@gmail.com>
2020-04-17 21:41:12 -0300

    fixed bug

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ hadoop dfs -mkdir /output1
 hadoop dfs -mkdir /input
 
 hadoop fs -copyFromLocal  ./data/apk.csv /input/.
-hadoop fs -copyFromLocal  ./data/reduced_reviews.csv /input/.
-hadoop fs -copyFromLocal  ./data/restaurant.csv /input/.
+hadoop fs -copyFromLocal  ~/Desktop/review.csv /input/.
+hadoop fs -copyFromLocal  ~/Desktop/restaurant.csv /input/.
 
 hadoop fs -rm /output1/output2.tsv
 hadoop fs -rm /output1/output3.tsv
 
-hadoop jar ./target/KASR-Recommendation-1.0-SNAPSHOT.jar runner.JobRunner1 /input/reduced_reviews.csv /output
+hadoop jar ./target/KASR-Recommendation-1.0-SNAPSHOT.jar runner.JobRunner1 /input/review.csv /output
 hadoop fs -mv /output/part-r-00000 /output1/output1.tsv 
 hadoop dfs -rmr /output
 
-hadoop jar ./target/KASR-Recommendation-1.0-SNAPSHOT.jar runner.JobRunner2 /input/reduced_reviews.csv /output
+hadoop jar ./target/KASR-Recommendation-1.0-SNAPSHOT.jar runner.JobRunner2 /input/review.csv /output
 hadoop fs -mv /output/part-r-00000 /output1/output2.tsv 
 hadoop dfs -rmr /output
 
